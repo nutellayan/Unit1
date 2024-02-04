598e9affcb49fe656e1ad41c085112eafe6c9514
nutellayen <157748332+nutellayan@users.noreply.github.com>
2024-02-04 19:12:46 +0000

    Umage ID 66a6d2559dbf

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:latest
-COPY ./target/classes/com /tmp/com
+COPY ./target/Unit1-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "com.napier.sem.App"]
+ENTRYPOINT ["java", "-jar", "Unit1-1.0-SNAPSHOT-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,7 +8,7 @@ import org.bson.Document;
 public class App {
     public static void main(String[] args) {
         // Use try-with-resources to automatically close MongoClient
-        try (MongoClient mongoClient = new MongoClient("localhost", 27000)) {
+        try (MongoClient mongoClient = new MongoClient("mongo-dbserver")) {
             // Get a database - will create when we use it
             MongoDatabase database = mongoClient.getDatabase("mydb");
             // Get a collection from the database
