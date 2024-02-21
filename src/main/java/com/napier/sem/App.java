72d2ae4ddc11b751d25cf0fad5556324b990faac
nutellayen <157748332+nutellayan@users.noreply.github.com>
2024-02-21 22:23:10 +0000

    use-cases 1 folder

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -76,6 +76,25 @@ public class App {
             return null;
         }
     }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
     /**
      * Disconnect from the MySQL database.
      */
@@ -102,6 +121,9 @@ public class App {
 
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Print salaries
+        a.printSalaries(employees);
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
